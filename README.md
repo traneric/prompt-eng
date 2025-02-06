c370139843913dda78056fe5042f3689004f66bc
Fernando Koch <kochf@fau.edu>
2025-02-05 19:14:42 -0500

    First release

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# prompt-eng
-Lessons about Prompt Engineering
+
+
+# Prompt Engineering Lessons
+
+Prompt engineering is a rapidly growing discipline focused on developing and optimizing prompts to maximize the effectiveness of language models (LMs) across various applications and research fields. This site serves as a comprehensive guide designed to help students, researchers, and developers master prompt engineering through theory, practical exercises, and hands-on experience using Python code and Jupyter notebooks.
+
+We provide:
+
+* **Techniques and Methods**: Master key prompting techniques, including zero-shot, few-shot, chain-of-thought, and self-consistency prompting.
+* **Model-Specific Customization**: Learn how to create effective prompts tailored for different models such as Llama, Phi, Qwen and others.
+* **Hands-on Experience**: experiement with different Prompt Experience Techniques through Jupyter notebook and Python code.
+
+# Configure your Enviroment
+
+Our exercises can be executed from Jupyter Notebooks that you can setup in different ways:
+* **use your computer**, e.g. using [Visual Studio Code](https://code.visualstudio.com/docs/datascience/jupyter-notebooks) or [installing Jupyter Lab](https://jupyterlab.readthedocs.io/en/stable/getting_started/installation.html)
+* **use [MyBinder.org](http://mybinder.org)**: we provide links to Binder Notebooks in every exercise
+
+To install on your computer, you first have to clone the code from Github:
+
+```bash
+git clone https://github.com/GenILab-FAU/prompt-eng.git prompt-eng
+```
+
+In both case (your computer of using MyBinder) you have to configure the 
+
+
+
+# Prompt Engineering Techniques
+
+* [Zero-Shot](prompt-eng/zero_shot.ipynb)
+* [Few-Shot](prompt-eng/few_shot.ipynb)
+* [Prompt Template](prompt-eng/prompt_template.ipynb)
+* [Chain-of-Thought](prompt-eng/chain_of_thought.ipynb)
+
+# References
+
+* [Meta How-To Guides: Prompting](https://www.llama.com/docs/how-to-guides/prompting/)
--- prompt-eng/pipeline.py
+++ prompt-eng/pipeline.py
@@ -1,20 +1,57 @@
 import requests
 import json
+import os
 import time
 
-def model_orch(url, payload, api_key=None):
+def load_config():
+    with open('config', 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                key, value = line.split('=', 1)
+                os.environ[key.strip()] = value.strip()
+    return
+
+def create_payload(model, prompt, **kwargs):
+    payload = {
+        "model": model,
+        "prompt": prompt, 
+        "stream": False,
+    }
+
+    if kwargs:
+        payload["options"] = {key: value for key, value in kwargs.items()}
     
+    return payload
+
+
+def model_req(payload):
+    # CUT-SHORT Condition
+    try:
+        load_config()
+    except:
+        return 0, f"!!ERROR!! Problem loading CONFIG"
+
+    url = os.getenv('URL', None)
+    api_key = os.getenv('API_KEY', None)
+    result = delta = response = None
+
     headers = dict()
     headers["Content-Type"] = "application/json"
     if api_key: headers["Authorization"] = f"Bearer API_KEY"
-    start_time = time.time()
-    response = requests.post(url, data=json.dumps(payload), headers=headers)
-    end_time = time.time()
+
+    # Send out request to Model Provider
+    try:
+        start_time = time.time()
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        delta = time.time() - start_time
+    except:
+        result = f"!!ERROR!! Request failed! Have you setup the URL and API_KEY properly ({url})?"
 
     # Checking the response and extracting the 'response' field
-    if response.status_code == 200:
+    if response and response.status_code == 200:
         result = response.json().get('response', 'No response field found')
-        print(result)
-        print(f"Time taken: {end_time - start_time:.2f} secs")
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
+    elif response:
+        result = f"!!ERROR!! HTTP Response={response.status_code}, {response.text}"
+    
+    return round(delta,3), result
